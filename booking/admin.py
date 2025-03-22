59fe01eb380edbb47b7018f96717308602ec5243
xtsvirko <oleksandr.tsvirko@gmail.com>
2025-03-22 15:31:58 +0100

    feat: add Facility and Booking models to admin panel

--- booking/admin.py
+++ booking/admin.py
@@ -1,3 +1,24 @@
 from django.contrib import admin
+from .models import Facility, Booking
 
-# Register your models here.
+
+@admin.register(Facility)
+class FacilityAdmin(admin.ModelAdmin):
+    list_display = ("name", "location", "capacity")
+    search_fields = ("name", "location")
+    ordering = ("name",)
+
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = (
+        "user",
+        "facility",
+        "booking_date",
+        "status",
+        "created_at",
+    )
+    list_filter = ("status", "booking_date", "facility")
+    search_fields = ("user__username", "user__email", "facility__name")
+    ordering = ("-created_at",)
+    autocomplete_fields = ("user", "facility")
