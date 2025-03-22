4025ce8294cb9551c50c9b4d9acbe9a3851ba544
xtsvirko <oleksandr.tsvirko@gmail.com>
2025-03-22 15:55:51 +0100

    feat: add booking inline to facility admin

--- booking/admin.py
+++ booking/admin.py
@@ -2,11 +2,20 @@ from django.contrib import admin
 from .models import Facility, Booking
 
 
+class BookingInline(admin.TabularInline):
+    model = Booking
+    extra = 0
+    readonly_fields = ("user", "booking_date", "status", "created_at")
+    can_delete = False
+    show_change_link = True
+
+
 @admin.register(Facility)
 class FacilityAdmin(admin.ModelAdmin):
     list_display = ("name", "location", "capacity")
     search_fields = ("name", "location")
     ordering = ("name",)
+    inlines = [BookingInline]
 
 
 @admin.register(Booking)
