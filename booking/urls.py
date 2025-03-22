8fc72fc0b69688a094de9e91eb5d090f00935401
xtsvirko <oleksandr.tsvirko@gmail.com>
2025-03-22 15:56:56 +0100

    feat: create facility list view, create a template to it and register endpoint

--- booking/urls.py
+++ booking/urls.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 from django.urls import path
 
+from booking.views import FacilityListView
 
 app_name = "booking"
 urlpatterns = [
-    path("", admin.site.urls),
+    path("facilities/", FacilityListView.as_view(), name="facility_list"),
 ]
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.views import generic
+
+from booking.models import Facility
+
 
 # Create your views here.
+class FacilityListView(generic.ListView):
+    model = Facility
+    paginate_by = 5
+    template_name = "booking/facility_list.html"
